f227c15afe0b821b3b66684db98d25f6ea645e1a
cs2812 <cs786786786@gmail.com>
2023-05-23 00:28:18 +0530

    add functionality

--- index.js
+++ index.js
@@ -6,56 +6,101 @@ let loader_container = document.getElementById("loader-container");
 let logo_upload_button = document.getElementById("logo-upload-button");
 let logo_upload_input = document.getElementById("logo-upload-input");
 let upload_logo = document.getElementById("upload-logo");
+let umbrella_uploaded_logo = document.getElementById("umbrella-uploaded-logo");
+let upload_loader = document.getElementById("uploade-loader");
+let isLogoAdded = false;
+let currentColor = "#2db3e5";
+let currentImage = "./assets/Blue.png"
+upload_logo.style.fill = "white";
 
-function showLoader() {
+function showLoader(color) {
+  umbrella_image.style.display = "none";
+  umbrella_uploaded_logo.style.display = "none";
+  document.getElementById("loader").style.fill = color ? color : currentColor;
+  upload_logo.style.display = "none";
+  upload_loader.style.display = "block";
+  upload_loader.style.backgroundColor = "transparent";
+  upload_loader.style.fill = "white";
   loader_container.style.opacity = 1;
 }
 
-function hideLoader() {
+function hideLoader(image) {
+  upload_logo.style.display = "block";
+  upload_loader.style.display = "none";
   loader_container.style.opacity = 0;
+  umbrella_image.src = image?image:currentImage;
+  umbrella_image.style.display = "block";
+  umbrella_uploaded_logo.style.display = "block";
 }
 
+// Loading handler function
 function handleLogoUpload(event) {
+  umbrella_uploaded_logo.style.display = "none";
   const file = event.target.files[0];
+  if (file.size > 5242880) {
+    alert("File Size should not be greter than 5MB");
+    return;
+  }
   const reader = new FileReader();
   reader.readAsDataURL(file);
   reader.onloadend = () => {
-    //   const uploadedLogo = document.getElementById("uploaded-logo");
-    //   uploadedLogo.src = reader.result;
-    //   uploadedLogo.style.display = "block";
-    //   umbrellaImage.style.zIndex = "-1";
-    console.log(reader);
+    isLogoAdded = true;
+    umbrella_image.style.display = "none";
+    showLoader();
+    setTimeout(() => {
+      hideLoader();
+      umbrella_uploaded_logo.src = reader.result;
+    }, 3000);
   };
 }
+logo_upload_input.addEventListener("change", handleLogoUpload);
 
-logo_upload_input.addEventListener("change", handleLogoUpload)
-
+//Blue Umbrella
 blue_btn.addEventListener("click", () => {
-  umbrella_image.src = "./assets/Blue.png";
-  logo_upload_button.style.backgroundColor = "#2db3e5";
-  upload_logo.style.backgroundColor = "#2db3e5";
+  currentColor = "#2db3e5";
+  logo_upload_button.style.backgroundColor = currentColor;
+  upload_logo.style.backgroundColor = currentColor;
   document.body.style.backgroundColor = "#c5e0dc";
-
-  //   umbrella_image.style.display = "none";
-  // //   showLoader();
-  //   setTimeout(() => {
-  //     // hideLoader();
-  //     umbrella_image.style.display = "block";
-  //   }, 3000);
+  if (!isLogoAdded) {
+    umbrella_image.src = "./assets/Blue.png";
+  } else {
+    showLoader("#2db3e5");
+    setTimeout(() => {
+      hideLoader("./assets/Blue.png");
+    }, 3000);
+  }
 });
 
+//Yellow Umbrella
 yellow_btn.addEventListener("click", () => {
-  umbrella_image.src = "./assets/Yello.png";
-  logo_upload_button.style.backgroundColor = "#fed144";
-  upload_logo.style.backgroundColor = "#fed144";
+  currentColor = "#fed144";
+  upload_logo.style.backgroundColor = currentColor;
+  logo_upload_button.style.backgroundColor = currentColor;
   document.body.style.backgroundColor = "#f7e09e";
+  if (!isLogoAdded) {
+    umbrella_image.src = "./assets/Yello.png";
+  } else {
+    showLoader("#fed144");
+    setTimeout(() => {
+      hideLoader("./assets/Yello.png");
+    }, 3000);
+  }
 });
 
+//Pink Umbrella
 pink_btn.addEventListener("click", () => {
-  umbrella_image.src = "./assets/Pink.png";
-  logo_upload_button.style.backgroundColor = "#da358c";
-  upload_logo.style.backgroundColor = "#da358c";
+  currentColor = "#da358c";
+  logo_upload_button.style.backgroundColor = currentColor;
+  upload_logo.style.backgroundColor = currentColor;
   document.body.style.backgroundColor = "#f4c4c4";
+  if (!isLogoAdded) {
+    umbrella_image.src = "./assets/Pink.png";
+  } else {
+    showLoader("#da358c");
+    setTimeout(() => {
+      hideLoader("./assets/Pink.png");
+    }, 3000);
+  }
 });
 
 logo_upload_button.addEventListener("click", () => {
